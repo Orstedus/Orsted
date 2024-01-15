14de50b6aeec241cf5deadae306048a2ecdacfec
Orsted <59316766+Orstedus@users.noreply.github.com>
2024-01-15 22:56:10 +0200

    setup gh-pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import '../styles/style.scss'
+import avatar from '../materials/ava.gif'
+import discord from '../materials/disik.png'
+import steam from '../materials/steam.png'
+import telegram from '../materials/telega.png'
+import github from '../materials/github.png'
 
 export const Header = () => {
   return (
     <div className='header'>
-        <img src='../materials/ava.gif' className='avatar' alt='avatar'/>
+        <img src={avatar} className='avatar' alt='avatar'/>
         <p className='nickName'>Orsted</p>
         <p className='description'>React JS developer</p>
         <p className='description'>14 Years Old</p>
         <p className='description'>Kyiv, Ukraine</p>
         <div className='comps'>
-          <img src='../../materials/disik.png' onClick={() => window.location = 'https://discord.gg/DHs3vMkYX7'}  className='comp' alt='discord'/>
-          <img src='../../materials/steam.png' onClick={() => window.location = 'https://steamcommunity.com/id/Krefenchik/'} className='comp' alt='steam'/>
-          <img src='../../materials/telega.png' onClick={() => window.location = 'https://t.me/+2jeMR2_jIPBiY2Yy'} className='comp' alt='telegram'/>
-          <img src='../../materials/github.png' onClick={() => window.location = 'https://github.com/Orstedus'} className='comp' alt='github'/>
+          <img src={discord} onClick={() => window.location = 'https://discord.gg/DHs3vMkYX7'}  className='comp' alt='discord'/>
+          <img src={steam} onClick={() => window.location = 'https://steamcommunity.com/id/Krefenchik/'} className='comp' alt='steam'/>
+          <img src={telegram} onClick={() => window.location = 'https://t.me/+2jeMR2_jIPBiY2Yy'} className='comp' alt='telegram'/>
+          <img src={github} onClick={() => window.location = 'https://github.com/Orstedus'} className='comp' alt='github'/>
         </div>
       </div>
   )
